5bd818b8b366d65f21ae4819db57199f4d57b4f5
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-07-28 09:19:54 +0200

    some Pool made.

---
+++ ISO-DesignerProjects/Workspace/DefaultPool/Output/DefaultPool.2.c
@@ -0,0 +1,49 @@
+// ISO-Designer ISO 11783   Version 5.6.0.5206 Jetter AG
+// Do not change!
+
+#include "DefaultPool.c.h"
+
+const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[] = {
+	     1,  // WorkingSet_0_Offset
+	    21,  // Macro_1_Offset
+	    34,  // Macro_2_Offset
+	    47,  // DataMask_1000_Offset
+	    67,  // DataMask_1001_Offset
+	    87,  // Container_3000_Offset
+	   109,  // SoftKeyMask_4000_Offset
+	   139,  // SoftKey_5000_Offset
+	   152,  // SoftKey_5001_Offset
+	   165,  // SoftKey_5002_Offset
+	   178,  // SoftKey_5003_Offset
+	   191,  // SoftKey_5004_Offset
+	   204,  // SoftKey_5005_Offset
+	   217,  // SoftKey_5006_Offset
+	   230,  // SoftKey_5007_Offset
+	   243,  // SoftKey_5008_Offset
+	   256,  // SoftKey_5009_Offset
+	   269,  // SoftKey_5010_Offset
+	   282,  // SoftKey_5011_Offset
+	   295,  // Button_6000_Offset
+	   322,  // Button_6001_Offset
+	   349,  // OutputString_11000_Offset
+	   378,  // OutputString_11001_Offset
+	   407,  // OutputString_11002_Offset
+	   436,  // OutputString_11003_Offset
+	   465,  // OutputString_11004_Offset
+	   494,  // OutputString_11005_Offset
+	   523,  // OutputString_11006_Offset
+	   552,  // OutputString_11007_Offset
+	   581,  // OutputString_11008_Offset
+	   610,  // OutputString_11009_Offset
+	   639,  // OutputString_11010_Offset
+	   668,  // OutputString_11011_Offset
+	   697,  // OutputString_11012_Offset
+	   726,  // OutputString_11013_Offset
+	   756,  // OutputString_11014_Offset
+	   786,  // OutputString_11015_Offset
+	   816,  // OutputString_11016_Offset
+	   846,  // OutputString_11017_Offset
+	   875,  // OutputString_11018_Offset
+	   904,  // FontAttributes_23000_Offset
+	   912,  // FontAttributes_23001_Offset
+};  // isoOP_DefaultPool_Offset
